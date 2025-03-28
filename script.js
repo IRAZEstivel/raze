3a77e328b1d0bc35b0e15f4d5ee92fd37627f942
IRAZEstivel <161456781+IRAZEstivel@users.noreply.github.com>
2025-03-28 12:29:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#ferrari', { delay: 500 });
+ScrollReveal().reveal('#tesla', { delay: 500 });
+ScrollReveal().reveal('#lamborghini', { delay: 500 });
+ScrollReveal().reveal('#porsche', { delay: 500 });
